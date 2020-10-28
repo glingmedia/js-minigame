3c2d0a503deff569a5a080322126136ef800a154
glingmedia <jonas.glingert@medieinstitutet.se>
2020-10-28 23:42:30 +0100

    scripting start

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function myFunction() {
-    let 
-    if (condition) {
+    let questionPrompt = 1;
+    if (questionPrompt==1) {
+        let question1 = prompt("You are on top of a mountain, what do you want to do?", "Jump or Go down")
+        if (question1 !== Jump) {
+            alert(You just died! Why jump?)
+
+        }
         
     }
 
