b34cdc84a0dbc186f17828e3d0cb042f4819970e
glingmedia <jonas.glingert@medieinstitutet.se>
2020-10-29 13:49:22 +0100

    try now

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ function miniGame() {
     let questionPrompt = 1;
     if (questionPrompt==1) {
         let question1 = prompt("You are on top of a mountain, what do you want to do?", "Jump or Go down")
-        if (question1 !== Jump) {
+        if (question1 !== null) {
+            if(question1==Jump) {
             alert(You just died! Why jump?);
         }
         if (question1==Go down) {
             alert(You go down the mountain);
             questionPrompt = 2;
         }
+    }
         
     }
 
