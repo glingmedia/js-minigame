9b98fa8528a8aada7fbd1587558579468da56ff0
glingmedia <jonas.glingert@medieinstitutet.se>
2020-10-29 17:14:23 +0100

    updated story

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function miniGame() {
     let questionPrompt = 1;
     if (questionPrompt==1) {
-        let question1 = prompt("You are on top of a mountain, what do you want to do?", "'jump' or 'go down'");
+        let question1 = prompt("Du befinner dig på entreplan i skolan, hur kommer du upp till våning 4?", "'hissen' or 'trappan'");
         if (question1 !== null) {
-            if(question1=='jump') {
-            alert('You just died! Why jump?');
+            if(question1=='hissen') {
+            alert('Din latmask! Hissen krånglar. Du fastnade och missar hela skoldagen... GAME OVER');
         }
-        if (question1=='go down') {
-            alert('You go down the mountain');
+        if (question1=='trappan') {
+            alert('Bra val! Lite motion i vardagen kan ju inte skada...');
             questionPrompt = 2;
         }
-        if (question1 != 'jump' && question1 != 'go down') {
-            alert('Wrong input');
+        if (question1 != 'hissen' && question1 != 'trappan') {
+            alert('Fel fel fel. Välj hissen eller trappan');
         }
     }
     }
