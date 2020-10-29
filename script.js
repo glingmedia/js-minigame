c35c62466ab98f5479b1977ce832fb89f08a9313
glingmedia <jonas.glingert@medieinstitutet.se>
2020-10-29 20:01:32 +0100

    even more script updates

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function miniGame() {
+    /** Fråga nr 1 */
     let questionPrompt = 1;
     if (questionPrompt==1) {
         let question1 = prompt("Du befinner dig på entreplan i skolan, hur kommer du upp till våning 4?", "hissen eller trappan");
         if (question1 !== null) {
             if(question1=='hissen') {
-            alert('Din latmask! Hissen krånglar. Du fastnade och missar hela skoldagen... GAME OVER');
+            alert('Din latmask! Hissen krånglar. Du fastnade och missar hela skoldagen. GAME OVER');
         }
         if (question1=='trappan') {
             alert('Bra val! Lite motion i vardagen kan ju inte skada.');
@@ -16,6 +17,7 @@ function miniGame() {
     }
 }
 
+    /** Fråga nr 2 */
     if(questionPrompt==2) {
         let question2 = prompt("Du befinner dig vid entredörren på vån 4.", "Vad är koden? Här får du ingen guidning!");
         if (question2 != null) {
@@ -30,6 +32,7 @@ function miniGame() {
     }
 }
 
+    /** Fråga nr 3 */
     if(questionPrompt==3) {
         let question3 = prompt("Du har nu gått igenom dörren med kodlåset. Åt vilket håll vill du gå?", "vänster, rakt eller höger");
         if (question3 !== null) {
@@ -40,12 +43,13 @@ function miniGame() {
             alert('Du gick rätt in en informationsdisk! GAME OVER');
         }
         if (question3=='höger') {
-            alert('Snyggt jobbat! Du hittade rätt och kom i tid! En rolig dag med stora utmaningar väntar...');
-            questionPrompt = 4;
+            alert('Snyggt jobbat! Du hittade rätt och kom i tid! En utmanande dag väntar...');
+            // Lägg in getelementbytid här
         }
         if (question3 != 'vänster' && question3 != 'rakt' && question3 != 'höger') {
             alert('Så kan man ju inte skriva! Skriv "vänster", "rakt" eller "höger"');
         }
     }
 }
+
 }
